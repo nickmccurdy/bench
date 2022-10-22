fd860ea567eca65cefe93e56b57bf8aa731ee5ba
Nick McCurdy <nick@nickmccurdy.com>
2022-10-22 05:42:57 -0400

    Update Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,11 @@ import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    open: true,
+  },
   build: {
     target: "es2022",
+    sourcemap: true,
   },
 })
