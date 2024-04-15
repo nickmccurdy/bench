14f01ccc64e896e00c4264ada1cddf1a2956fe7b
Nick McCurdy <nick@nickmccurdy.com>
2024-04-15 12:25:00 -0400

    Use prettier-config-a11y

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,20 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
-  plugins: [react()],
-  server: {
-    open: true,
-  },
-  build: {
-    sourcemap: true,
-    commonjsOptions: {
-      include: [],
-    },
-  },
-  optimizeDeps: {
-    disabled: false,
-  },
-})
+	base: "./",
+	plugins: [react()],
+	server: {
+		open: true,
+	},
+	build: {
+		sourcemap: true,
+		commonjsOptions: {
+			include: [],
+		},
+	},
+	optimizeDeps: {
+		disabled: false,
+	},
+});
